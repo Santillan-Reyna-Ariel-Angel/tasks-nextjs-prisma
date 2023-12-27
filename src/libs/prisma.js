f1287138578e2acbad44dc7c7e953baabc695767
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2023-12-27 16:04:29 -0400

    Conexion a la bd con prisma

---
+++ src/libs/prisma.js
@@ -0,0 +1,3 @@
+import { PrismaClient } from '@prisma/client'; // Importamos la clase que nos permitira conectarnos a la base de datos
+
+export const prisma = new PrismaCleint(); // De estamanera nos podemos conectar a la base de datos
